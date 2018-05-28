d2b0f3da84d3bd5c19e0b7302633f757206cfb47
Theresia Bruns <theresia.bruns@student.hpi.uni-potsdam.de>
2018-05-28 21:05:20 +0200

    Initial Commit

---
+++ test-appTests/test_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_appTests.swift
+//  test-appTests
+//
+//  Created by theresia.bruns on 28.05.18.
+//  Copyright © 2018 theresia.bruns. All rights reserved.
+//
+
+import XCTest
+@testable import test_app
+
+class test_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
