248c3e7505dd4c8eadb20c1f1a307680987323d9
Theresia Bruns <theresia.bruns@student.hpi.uni-potsdam.de>
2018-05-30 17:26:42 +0200

    Set up app

--- test-app/ViewController.swift
+++ test-app/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var howMuch: UITextField!
+    @IBOutlet weak var of: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,24 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func calculateProcent(howMuch: UITextField, of: UITextField) -> Int{
+        var procent = 0
+      //  procent = (Int(howMuch!) * 100) / Int(of!)
+        return procent
+    }
+    
+    @IBAction func showResult (sender: UIButton) {
+        var procent = calculateProcent(howMuch: howMuch, of: of)
+        
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message:
+            "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 
 
